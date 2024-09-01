6564c6e4e3821fdb21772360808f8dca8a87036b
Lucas Bassora da Silva <lucasbassora07@gmail.com>
2024-09-01 12:57:53 -0300

    sequencia de fibonacci

---
+++ fibonacci.py
@@ -0,0 +1,19 @@
+primeiroValor = 0
+segundoValor = 1
+
+fibonacci = []
+
+numero = 2
+
+
+for i in range(10):
+    fibonacci.append(primeiroValor)
+    terceiroValor = primeiroValor + segundoValor
+    primeiroValor = segundoValor
+    segundoValor = terceiroValor
+
+
+if numero in fibonacci:
+    print(f'O numero {numero} está presente na sequencia')
+else: 
+    print(f'O numero {numero} não esta presente na sequencia')
